6898dfd13027c54b3b135d2e5e2607cd0b1b44d3
hap196 <haileyrpan@gmail.com>
2025-02-01 13:04:08 -0500

    Initial Commit

---
+++ hackatbrown/hackatbrownApp.swift
@@ -0,0 +1,17 @@
+//
+//  hackatbrownApp.swift
+//  hackatbrown
+//
+//  Created by Hailey Pan on 2/1/25.
+//
+
+import SwiftUI
+
+@main
+struct hackatbrownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
